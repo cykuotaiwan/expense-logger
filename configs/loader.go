247c74b67b0a68369b0629936af4f725d6e96e94
cykuo <cykuotw45@gmail.com>
2023-01-19 12:34:41 -0500

    [fix] use absolute dir

--- configs/loader.go
+++ configs/loader.go
@@ -2,12 +2,14 @@ package configs
 
 import (
 	"log"
+	"os"
 
 	env "github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := env.Load(".env")
+	dir := os.Getenv("GOPATH") + "/src/expense-logger/.env"
+	err := env.Load(dir)
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
