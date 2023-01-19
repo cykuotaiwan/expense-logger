32915aff8eaf8ca97ad2ad2e3244848a66bf7259
cykuo <cykuotw45@gmail.com>
2023-01-19 15:14:56 -0500

    [test] add TestInsertExpense

--- web/app/models/expense/insert_test.go
+++ web/app/models/expense/insert_test.go
@@ -4,12 +4,11 @@ import (
 	config "expense-logger/configs"
 	db "expense-logger/web/app/models"
 	exp "expense-logger/web/app/models/expense"
-	"fmt"
-
 	"testing"
+	"time"
 )
 
-func TestInsert(t *testing.T) {
+func TestInsertItem(t *testing.T) {
 	// db connection
 	config.Init()
 	db.Init()
@@ -18,24 +17,77 @@ func TestInsert(t *testing.T) {
 	// setup test data
 	var newItems = []exp.Item{
 		{
-			ItemID: 1,
-			Name:   "Potatoes",
-			Price:  7.99,
-			Count:  20,
+			Name:  "Potatoes",
+			Price: 249,
+			Unit:  exp.Ulb,
+			Count: 3,
 		},
 		{
-			ItemID: 9887,
-			Name:   "Milk",
-			Price:  13.98,
-			Count:  1,
+			Name:  "Milk",
+			Price: 1398,
+			Unit:  exp.Uea,
+			Count: 1,
 		},
 	}
-	// insert items
-	res, err := exp.InsertItem(newItems)
-	fmt.Println(res)
-	if err != nil {
-		fmt.Println(err.Error())
-		t.Errorf(err.Error())
+	// main test
+	t.Run("nil", func(t *testing.T) {
+		res, err := exp.InsertItem(nil)
+		if err != nil || res != nil {
+			t.Errorf(err.Error())
+		}
+	})
+	t.Run("empty array", func(t *testing.T) {
+		res, err := exp.InsertItem([]exp.Item{})
+		if err != nil || res != nil {
+			t.Errorf(err.Error())
+		}
+	})
+	t.Run("empty items", func(t *testing.T) {
+		res, err := exp.InsertItem([]exp.Item{{}, {}})
+		if err != nil || res != nil {
+			t.Errorf(err.Error())
+		}
+	})
+	// test insert items
+	t.Run("two items", func(t *testing.T) {
+		res, err := exp.InsertItem(newItems)
+		if err != nil || res == nil {
+			t.Errorf(err.Error())
+		}
+	})
+
+}
+
+func TestInsertExpense(t *testing.T) {
+	// db connection
+	config.Init()
+	db.Init()
+	defer db.Close()
+
+	// setup test data
+	var newExpense = exp.Expense{
+		DateTime: time.Now(),
+		ShopName: "Loblaws",
+		Total:    2011,
 	}
 
+	// main test
+	t.Run("nil", func(t *testing.T) {
+		res, err := exp.InsertExpense(nil)
+		if err != nil || res != nil {
+			t.Errorf(err.Error())
+		}
+	})
+	t.Run("empty", func(t *testing.T) {
+		res, err := exp.InsertExpense(&exp.Expense{})
+		if err != nil || res != nil {
+			t.Errorf(err.Error())
+		}
+	})
+	t.Run("valid value", func(t *testing.T) {
+		res, err := exp.InsertExpense(&newExpense)
+		if err != nil || res == nil {
+			t.Errorf(err.Error())
+		}
+	})
 }
