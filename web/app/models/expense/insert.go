78a73d671798150d74d36f9a76e443ee87871d3c
cykuo <cykuotw45@gmail.com>
2023-01-19 12:35:24 -0500

    [feat] model insert

--- web/app/models/expense/insert.go
+++ web/app/models/expense/insert.go
@@ -1,1 +1,35 @@
 package expense
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func InsertItem(newItems []Item) (*mongo.InsertManyResult, error) {
+	items := make([]interface{}, len(newItems))
+	for i := range newItems {
+		items[i] = newItems[i]
+	}
+	res, err := itemCollection.InsertMany(
+		context.TODO(),
+		items,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func InsertExpense(newExpense *Expense) (*mongo.InsertOneResult, error) {
+	res, err := expCollection.InsertOne(
+		ctx,
+		newExpense,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
