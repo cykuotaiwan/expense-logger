9d2949682c132aac84102fea9c9f021e66563275
cykuo <cykuotw45@gmail.com>
2023-01-19 21:39:01 -0500

    [feat] add expense get functions

--- web/app/models/expense/get.go
+++ web/app/models/expense/get.go
@@ -1,21 +1,44 @@
 package expense
 
 import (
+	"context"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GetExpenseLatest(cnt int8, offset int8) ([]Expense, error) {
+func GetExpenseLatest(cnt uint8, offset uint8) ([]Expense, error) {
 	option := options.Find()
 	option.SetSort(bson.M{"$natural": -1})
 	option.SetLimit(int64(cnt))
 	filter := bson.D{}
-	cur, err := expCollection.Find(ctx, filter, option)
+
+	cursor, err := expCollection.Find(context.TODO(), filter, option)
 	if err != nil {
 		return nil, err
 	}
+
 	var res []Expense
-	if err = cur.All(ctx, &res); err != nil {
+	if err = cursor.All(ctx, &res); err != nil {
+		return nil, err
+	}
+
+	return res[offset:], nil
+}
+
+func GetItemLatest(cnt uint8, offset uint8) ([]Item, error) {
+	option := options.Find()
+	option.SetSort(bson.M{"$natural": -1})
+	option.SetLimit(int64(cnt))
+	filter := bson.D{}
+
+	cursor, err := itemCollection.Find(context.TODO(), filter, option)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []Item
+	if err = cursor.All(ctx, &res); err != nil {
 		return nil, err
 	}
 	return res[offset:], nil
