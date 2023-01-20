bfb2c4b1f53b40b4f833db2c507cc272514c3f14
cykuo <cykuotw45@gmail.com>
2023-01-19 20:43:31 -0500

    [test] insert item and expense

--- web/app/models/expense/insert_test.go
+++ web/app/models/expense/insert_test.go
@@ -6,6 +6,8 @@ import (
 	exp "expense-logger/web/app/models/expense"
 	"testing"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestInsertItem(t *testing.T) {
@@ -88,6 +90,50 @@ func TestInsertExpense(t *testing.T) {
 		res, err := exp.InsertExpense(&newExpense)
 		if err != nil || res == nil {
 			t.Errorf(err.Error())
+		}
+	})
+}
+
+func TestInsertItemExpense(t *testing.T) {
+	// db connection
+	config.Init()
+	db.Init()
+	defer db.Close()
+
+	// setup test data
+	var newItems = []exp.Item{
+		{
+			Name:  "Potatoes",
+			Price: 249,
+			Unit:  exp.Ulb,
+			Count: 3,
+		},
+		{
+			Name:  "Milk",
+			Price: 1398,
+			Unit:  exp.Uea,
+			Count: 1,
+		},
+	}
+	var newExpense = exp.Expense{
+		DateTime: time.Now(),
+		ShopName: "Loblaws",
+		Total:    2011,
+	}
+
+	t.Run("both item and expense", func(t *testing.T) {
+		resItem, errItem := exp.InsertItem(newItems)
+		if errItem != nil || resItem == nil {
+			t.Errorf(errItem.Error())
+		}
+
+		newExpense.ItemIDs = make([]primitive.ObjectID, 0, len(resItem.InsertedIDs))
+		for _, id := range resItem.InsertedIDs {
+			newExpense.ItemIDs = append(newExpense.ItemIDs, id.(primitive.ObjectID))
+		}
+		resExp, errExp := exp.InsertExpense(&newExpense)
+		if errExp != nil || resExp == nil {
+			t.Errorf(errExp.Error())
 		}
 	})
 }
