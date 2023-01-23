ad9013edd01b78baea8610c2ff3c6c96aa88ea6c
cykuo <cykuotw45@gmail.com>
2023-01-23 14:54:03 -0500

    [feat] add UpdateItem

--- web/app/models/expense/update.go
+++ web/app/models/expense/update.go
@@ -8,18 +8,50 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+func UpdateItem(newItem Item, itemId primitive.ObjectID) (*mongo.UpdateResult, error) {
+	filter := bson.M{
+		"_id": bson.M{"$eq": itemId},
+	}
+
+	update := bson.D{
+		{
+			Key: "$set",
+			Value: bson.M{
+				"name":      newItem.Name,
+				"price":     newItem.Price,
+				"count":     newItem.Count,
+				"unit":      newItem.Unit,
+				"expenseId": newItem.ExpenseID,
+			},
+		},
+	}
+
+	res, err := itemCollection.UpdateOne(
+		context.TODO(),
+		filter,
+		update,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
 func UpdateExpense(newExpense Expense, expId primitive.ObjectID) (*mongo.UpdateResult, error) {
 	filter := bson.M{
 		"_id": bson.M{"$eq": expId},
 	}
 
 	update := bson.D{
-		{Key: "$set", Value: bson.M{
-			"datetime": newExpense.DateTime,
-			"shopName": newExpense.ShopName,
-			"total":    newExpense.Total,
-			"itemIDs":  newExpense.ItemIDs,
-		}},
+		{
+			Key: "$set",
+			Value: bson.M{
+				"datetime": newExpense.DateTime,
+				"shopName": newExpense.ShopName,
+				"total":    newExpense.Total,
+				"itemIDs":  newExpense.ItemIDs,
+			},
+		},
 	}
 	res, err := expCollection.UpdateOne(
 		context.TODO(),
@@ -31,5 +63,4 @@ func UpdateExpense(newExpense Expense, expId primitive.ObjectID) (*mongo.UpdateR
 		return nil, err
 	}
 	return res, nil
-
 }
