bdd1b0a6a6cae7bdde5e50927edefe229343af76
cykuo <cykuotw45@gmail.com>
2023-01-23 14:18:58 -0500

    [feat] add UpdateExpense

--- web/app/models/expense/update.go
+++ web/app/models/expense/update.go
@@ -1,22 +1,28 @@
 package expense
 
 import (
+	"context"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func UpdateExpense(newExpense Expense, expId primitive.ObjectID) (*mongo.UpdateResult, error) {
-	filter := bson.D{
-		{Key: "id", Value: expId},
+	filter := bson.M{
+		"_id": bson.M{"$eq": expId},
 	}
+
 	update := bson.D{
-		{Key: "$set", Value: bson.D{
-			// something
+		{Key: "$set", Value: bson.M{
+			"datetime": newExpense.DateTime,
+			"shopName": newExpense.ShopName,
+			"total":    newExpense.Total,
+			"itemIDs":  newExpense.ItemIDs,
 		}},
 	}
 	res, err := expCollection.UpdateOne(
-		ctx,
+		context.TODO(),
 		filter,
 		update,
 	)
