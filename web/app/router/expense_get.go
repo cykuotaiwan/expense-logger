b16da0aa94c57d1f353b12c251431e4569ff63ac
cykuo <cykuotw45@gmail.com>
2023-03-17 20:55:57 -0400

    [feat] add GetExpenseByDate

--- web/app/router/expense_get.go
+++ web/app/router/expense_get.go
@@ -2,35 +2,79 @@ package router
 
 import (
 	"encoding/json"
+	exp "expense-logger/web/app/models/expense"
 	"io"
 	"net/http"
-
-	exp "expense-logger/web/app/models/expense"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-type expenseParam struct {
+type latestParam struct {
 	Count  int `json:"cnt"`
 	Offset int `json:"offset"`
 }
 
+type getByDateParam struct {
+	StartDate time.Time `json:"startDate"`
+	EndDate   time.Time `json:"endDate"`
+}
+
 func GetExpenseLatest(c *gin.Context) {
-	var param expenseParam
+	var param latestParam
 
 	if c.Request.Body != nil {
 		body, err := io.ReadAll(c.Request.Body)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, nil)
+			c.JSON(http.StatusInternalServerError, nil)
 			return
 		}
 		err = json.Unmarshal(body, &param)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+		if param.Offset < 0 || param.Count < 0 {
+			c.JSON(http.StatusBadRequest, nil)
+			return
+		}
 	} else {
 		param.Count = 8
 		param.Offset = 0
 	}
 
 	expenseSet, err := exp.GetExpenseLatest(uint8(param.Count), uint8(param.Offset))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": expenseSet,
+	})
+}
+
+func GetExpenseByDate(c *gin.Context) {
+	var param getByDateParam
+
+	if c.Request.Body != nil {
+		body, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+		err = json.Unmarshal(body, &param)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+	} else {
+		// one day
+		param.StartDate = time.Now().AddDate(0, 0, -1)
+		param.EndDate = time.Now()
+	}
+
+	expenseSet, err := exp.GetExpenseByDate(param.StartDate, param.EndDate)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, nil)
 		return
