de892e993907ea2bd525915a5fc6e5c141b7bf6a
cykuo <cykuotw45@gmail.com>
2023-03-17 20:57:04 -0400

    [feat] add GetItemLatest

---
+++ web/app/router/item_get.go
@@ -0,0 +1,44 @@
+package router
+
+import (
+	"encoding/json"
+	exp "expense-logger/web/app/models/expense"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetItemLatest(c *gin.Context) {
+	var param latestParam
+
+	if c.Request.Body != nil {
+		body, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+		err = json.Unmarshal(body, &param)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, nil)
+			return
+		}
+		if param.Offset < 0 || param.Count < 0 {
+			c.JSON(http.StatusBadRequest, nil)
+			return
+		}
+	} else {
+		param.Count = 8
+		param.Offset = 0
+	}
+
+	itemSet, err := exp.GetItemLatest(uint8(param.Count), uint8(param.Offset))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": itemSet,
+	})
+}
